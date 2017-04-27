23c50ff21c5d2c80ac099acb83ea05bc8438a2b8
notew7-PC\notew7 <contato@rafaelestevao.com.br>
2017-04-26 23:04:58 -0300

    Maratona xamarin intermediária

---
+++ MaratonaXamarin/MaratonaXamarin.Shared/UserApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaratonaXamarin.Shared
+{
+    public class UserApi
+    {
+        //private readonly MobileServiceClient _client;
+
+        //public UserApi()
+        //{
+        //    _client = new MobileServiceClient("http://hellomonkeys.azurewebsites.net");
+        //}
+    }
+}
---
+++ MaratonaXamarin/MaratonaXamarinAndroidApp/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MaratonaXamarinAndroidApp
+{
+    [Activity(Label = "MaratonaXamarinAndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var button = this.FindViewById<Button>(Resource.Id.btnCarregar);
+            var listview = this.FindViewById<ListView>(Resource.Id.lvwItens);
+
+            button.Click += (sender, e) =>
+            {
+
+            };
+        }
+
+    }
+}
+
